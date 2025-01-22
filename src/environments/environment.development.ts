cbd4195229cc1cd1457406887b8ea1120b9414ab
Kris Gray <kag56@cam.ac.uk>
2025-01-22 17:43:01 +0000

    Implement server-side rendering with Angular and update environment configurations

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+    production: false,
+    jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0IiwiZW1haWwiOiJ3ZWJzZXJ2ZXJAZW1haWwuY29tIiwiaWF0IjoxNzM3MTM1Mzc0LCJleHAiOjM1MDMxMjIwNDg5LCJhdWQiOiJsb2NhbGhvc3Q6MzAwMCIsImlzcyI6ImxvY2FsaG9zdDozMDAwIn0.XZ3KUvLJoQedQyUMS27ipQEgw2P0umdvSIHF8SzkbPE',
+};
